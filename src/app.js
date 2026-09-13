6b90c915310e401647d09845404631ca4112a20f
APURV CHOUDHARY <104256475+APURVTHECODER@users.noreply.github.com>
2026-09-13 20:23:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 
+const express = require("express");
+const _ = require("lodash");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const message = _.upperCase("hello gh500");
+  res.send(message);
+});
+
+app.listen(3000, () => {
+  console.log("GH500 lab running on port 3000");
+});
